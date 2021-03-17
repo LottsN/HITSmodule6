e9a0f7b0f237e37d16a1f3bb1df71ae13ccb7093
Amine32 <aminov.02@hotmail.com>
2021-03-17 20:50:18 +0200

    Add files via upload

---
+++ TASK1/js.js
@@ -0,0 +1,11 @@
+function createMap() {
+    background(51);
+    var n = document.getElementById("numb").value;
+
+    for (var i = 0; i < n; i++) {
+        for (var j = 0; j < n; j++) {
+            
+        }
+    }
+
+}
