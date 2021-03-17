be537fb4bcfa465f01a0c8513b0181e426a05283
Amine32 <aminov.02@hotmail.com>
2021-03-17 23:16:03 +0200

    path 0.2

--- TASK1/js.js
+++ TASK1/js.js
@@ -1,11 +1,5 @@
 function createMap() {
-    background(51);
     var n = document.getElementById("numb").value;
 
-    for (var i = 0; i < n; i++) {
-        for (var j = 0; j < n; j++) {
-            
-        }
-    }
-
+    window.alert(n);
 }
